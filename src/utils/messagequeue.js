a3129c597d178c44f15ac2e4b9a4c42b1d8b7aaa
leander006 <leanderdsilva06@gmail.com>
2023-08-14 21:39:01 +0530

    Added setup for rabbitmq

---
+++ src/utils/messagequeue.js
@@ -0,0 +1,41 @@
+const amqplib = require("amqplib");
+const { MESSAGE_BROKER_URL, EXCHANGE_NAME } = require("../config/serverConfig");
+
+const createChannel = async () => {
+  try {
+    const connection = await amqplib.connect(MESSAGE_BROKER_URL);
+    const channel = await connection.createChannel();
+    await channel.assertExchange(EXCHANGE_NAME, "direct", false);
+    return channel;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const subscribeMessage = async (channel, service, binking_key) => {
+  try {
+    const applicationQueue = await channel.assertQueue("QUEUE_NAME");
+    channel.bindQueue(applicationQueue.queue, EXCHANGE_NAME, binking_key);
+    channel.consume(applicationQueue.queue, (msg) => {
+      console.log(msg.content.toString());
+      channel.ack(msg);
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+const publishMessage = async (channel, binking_key, message) => {
+  try {
+    await channel.assertQueue("QUEUE_NAME");
+    await channel.publish(EXCHANGE_NAME, binking_key, Buffer.from(message));
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = {
+  createChannel,
+  subscribeMessage,
+  publishMessage,
+};
